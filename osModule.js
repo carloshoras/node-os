ab7ce8d52dd4fb30338cbefe491a555a8b93fbe3
carloshoras <carloshoras>
2024-01-09 20:43:59 +0100

    conversion MB

--- osModule.js
+++ osModule.js
@@ -5,7 +5,7 @@ function printOS() {
     console.log(`Versión: ${os.version()}`)
     console.log(`Arquitectura: ${os.arch()}`)
     console.log(`CPUs: ${os.cpus().length}`)
-    console.log(`Memoria Total: ${(os.totalmem()/10e6).toFixed(2)} MB`)
-    console.log(`Memoria Libre: ${(os.freemem()/10e6).toFixed(2)} MB`)
+    console.log(`Memoria Total: ${(os.totalmem()/1e6).toFixed(2)} MB`)
+    console.log(`Memoria Libre: ${(os.freemem()/1e6).toFixed(2)} MB`)
 }
 module.exports = printOS
